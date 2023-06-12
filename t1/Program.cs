d5796122f7dbd70ebac30b4ea901adfa9ecf4948
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 14:46:15 +0300

    t1 valmis

--- t1/Program.cs
+++ t1/Program.cs
@@ -15,3 +15,12 @@ objectPerson.SetAge(21);
 //objectPerson.SetName(Console.ReadLine());
 Console.WriteLine("Nimi="+objectPerson.GetName());
 Console.WriteLine("Ikä="+objectPerson.GetAge());
+
+//Person2 luokass Propertyt Name ja Age
+//korvaavat Person luokan privaatti muuttujat
+//ja niiden getter ja setter metodit
+Person2 objectPerson2 = new Person2();  
+objectPerson2.Age = 24;
+objectPerson2.Name = "Matti Virta";
+Console.WriteLine("Person2 nimi=" + objectPerson2.Name);
+Console.WriteLine("Person2 ikä="+objectPerson2.Age);
