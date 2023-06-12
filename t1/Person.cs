691b55a7fb4e08fa23809027a218602a5ad81abd
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 14:38:19 +0300

    t1 valmis

---
+++ t1/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1
+{
+    internal class Person
+    {
+        private String name;
+        private int age;
+   
+        public void SetName(String value)
+        {
+            name= value;
+        }
+        public String GetName()
+        {
+            return name;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        public void SetAge(int value)
+        {
+            age = value;
+        }
+    }
+}
---
+++ t1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Luo luokka nimeltä "Person" ja 
+ * siihen ominaisuudet "Name" ja "Age". 
+ * Luo sitten uusi Person-olio ja aseta sille 
+ * nimi ja ikä. Tulosta Person-olion 
+ * tiedot konsoliin.
+*/
+using t1;
+
+Person objectPerson = new Person();
+objectPerson.SetName("Teppo Testi");
+objectPerson.SetAge(21);
+//Console.WriteLine("Anna nimesi");
+//objectPerson.SetName(Console.ReadLine());
+Console.WriteLine("Nimi="+objectPerson.GetName());
+Console.WriteLine("Ikä="+objectPerson.GetAge());
