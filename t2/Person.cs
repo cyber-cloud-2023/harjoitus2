564efa557d34de205efccb23b63278c0ccf3e527
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 15:25:21 +0300

    t2 valmis

---
+++ t2/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t2
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public void Welcome(String message)
+        {
+            Console.WriteLine(message+" " + Age + " vuotias " + Name);
+        }
+    }
+}
---
+++ t2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using t2;
+
+Console.WriteLine("Hello, World!");
+Person person = new Person();
+person.Name = "Teppo Testi";
+person.Age = 22;
+person.Welcome("Terve");
+person.Welcome("Hello");
