3d25fb27f39d61088022e98c8ddf2a1ba18521a7
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-12 15:58:53 +0300

    t5 valmis

---
+++ t5/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t5
+{
+    internal class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+
+        //Muodostimet
+        public Book() { }
+        public Book(string n, string a, int p) { 
+            Name = n;
+            Author = a;
+            PublicationYear = p;
+        }
+    }
+}
---
+++ t5/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using t5;
+
+
+Book book = new Book();
+book.Name = "C# opas";
+book.Author = "Teppo Testi";
+book.PublicationYear = 2021;
+
+Book book2 = new Book("Java","Matti Virta",2019);
+
+Console.WriteLine("Ensimmäinen olio");
+Console.WriteLine(book.Name);
+Console.WriteLine(book.Author);
+Console.WriteLine(book.PublicationYear.ToString());
+
+Console.WriteLine("Toinen olio");
+Console.WriteLine(book2.Name);
+Console.WriteLine(book2.Author);
+Console.WriteLine(book2.PublicationYear.ToString());
